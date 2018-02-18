2948befa3c68cfdda0989a1459f358a6373b211c
Christopher Pelletier <chris.r.pelletier@gmail.com>
2018-02-17 17:21:57 -0800

    Added pymodm code

--- api/app.py
+++ api/app.py
@@ -1,18 +1,21 @@
 import os
 import socket
 from flask import Flask, redirect, url_for, request, render_template
-from pymongo import MongoClient
+from pymodm import connect, MongoModel, fields
 
 app = Flask(__name__)
+mongo_uri = 'mongodb://{}:{}/docker_app'.format(os.environ['MONGODB_HOST'], os.environ['MONGODB_PORT'])
+connect(mongo_uri)
 
-client = MongoClient(os.environ['MONGODB_HOST'])
-db = client.dockerApp
+class Todos(MongoModel):
+    title = fields.CharField(max_length=100)
+    created_on = fields.DateTimeField()
+    updated_on = fields.DateTimeField()
 
-@app.route('/')
-def hello():
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname} <br/>" 
-    return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname())
+@app.route('/api/todos')
+def todos(self):
+    return []
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, port=5000)
